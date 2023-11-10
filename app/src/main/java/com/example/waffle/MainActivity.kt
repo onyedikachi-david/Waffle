9c55329fdfb75f29e4963b473bd9017dab81ddca
David Anyatonwu <davidanyatonwu@gmail.com>
2023-11-10 03:21:25 +0100

    done

--- app/src/main/java/com/example/waffle/MainActivity.kt
+++ app/src/main/java/com/example/waffle/MainActivity.kt
@@ -1,35 +1,43 @@
 package com.example.waffle
 
+import android.net.Uri
+//import android.net.Uri
+import com.solana.mobilewalletadapter.clientlib.ActivityResultSender
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.waffle.ui.theme.WaffleTheme
-import com.example.waffle.composable.WalletConnectButton
+import com.example.waffle.composables.WalletConnectButton
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var activityResultSender = ActivityResultSender(this)
         setContent {
             WaffleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+//                    color = MaterialTheme.colorScheme.background
                 ) {
 //                    Greeting("Android")
                     Column(modifier = Modifier.padding(16.dp)) {
-                        WalletConnectButton(modifier = Modifier.align(Alignment.End))
+                        WalletConnectButton(
+                            identityUri = Uri.parse(application.getString((R.string.id_url))),
+                            iconUri = Uri.parse(application.getString(R.string.id_favicon)),
+                            identityName = application.getString(R.string.app_name),
+                            activityResultSender = activityResultSender,
+                            modifier = Modifier.align(Alignment.End))
                     }
                 }
             }
